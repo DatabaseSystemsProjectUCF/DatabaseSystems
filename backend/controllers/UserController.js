4eec58d9b6c629ce5ccdf27e6f603ce17c3ecd30
DaniOstos <101365912+DaniOstos@users.noreply.github.com>
2023-02-23 16:45:56 -0500

    finished register endpoint

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const connection = require("./../Database");
-
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,7 +12,7 @@ const login_handler = async (req, res) => {
   const { email, password } = req.body;
 
   // Query the database for the user with the specified username
-  const query = `SELECT * FROM user WHERE email = ?`;
+  const query = `SELECT * FROM users WHERE email = ?`;
   connection.query(query, [email], (err, results) => {
     if (err) throw err;
 
@@ -36,26 +35,46 @@ const login_handler = async (req, res) => {
     }
   });
 };
-  
+
 const register_handler = async (req, res) => {
   //get user information
-  const { email, password, first_name, last_name} = req.body;
+  const { email, password, first_name, last_name } = req.body;
   const hashedPass = await bcrypt.hash(password, saltRounds);
-  
-  //create query
+
+  //check if user will be super_admin
+  var level_id = 0;
+  const email_domain = email.substring(email.indexOf("@"), email.length);
+
+  //create queries
+  const s_query = `SELECT email FROM univ_emails WHERE email = ?`;
+  const u_query = `INSERT INTO univ_emails (email) VALUES (?)`;
   const query = `INSERT INTO users (email, password, first_name, last_name, level_id) VALUES (?, ?, ?, ?, ?)`;
-  const values = [ email, hashedPass, first_name, last_name, 2];
 
-  //execute query
-  connection.query(query, values, (error, result)=>{
-    if(error){
+  //execute query to check for email domains
+  connection.query(s_query, email_domain, (error, result) => {
+    if (error) {
       return res.status(401).send(error.sqlMessage);
+    } else {
+      if (result[0] == null) {
+        level_id = 2;
+        //execute query to insert email in univ_emails table
+        connection.query(u_query, email_domain, (error, result) => {
+          if (error) {
+            return res.status(401).send(error.sqlMessage);
+          }
+        });
+      }
+      //execute query to add new user
+      const values = [email, hashedPass, first_name, last_name, level_id];
+      connection.query(query, values, (error, result) => {
+        if (error) {
+          return res.status(401).send(error.sqlMessage);
+        } else {
+          return res.status(200).json(result);
+        }
+      });
     }
-    else{
-      return res.status(200).json(result);
-    }
-  })
-
-}
+  });
+};
 
 module.exports = { login_handler, register_handler };
