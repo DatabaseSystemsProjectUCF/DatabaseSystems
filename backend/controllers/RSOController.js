2014a561e90bf17f4b5c738c5430945c5c0af7c3
Juan Guarnizo <88952365+Ozymandias0912@users.noreply.github.com>
2023-03-01 14:08:56 -0500

    Modified join_rso_handler
    I tried to fix the code so the queries are executed in the right order.

--- backend/controllers/RSOController.js
+++ backend/controllers/RSOController.js
@@ -8,87 +8,87 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const join_rso_handler = async (req, res) => {
+  //get information from the request
+  const { id } = req.body;
+  const { rso_id } = req.param;
 
-const join_rso_handler = (req, res) => {
-    //get information from the request
-    const {id} = req.body;
-    const {rso_id} = req.param;
+  const verify_query1 = `SELECT * FROM students WHERE id = ?`;
+  const verify_query2 = `SELECT * FROM rso WHERE rso_id = ?`;
+  const query = `INSERT INTO joins (rso_id, id) VALUES (?, ?)`;
 
-    //verify the rso id and the student id are valid
-    const verify_query1 = `SELECT * FROM students WHERE id = ?`;
-    const verify_query2 = `SELECT * FROM rso WHERE rso_id = ?`;
-    const query = `INSERT INTO joins (rso_id, id) VALUES (?, ?)`;
+  //verify the student exists in the database
+  await connection.query(verify_query1, [id], (err, results) => {
+    if (err) throw err;
 
-    connection.query(verify_query1, [id], (err, results) => {
-        if(err) throw err;
+    //the student is not in the database
+    if (results[0] == null) {
+      res.status(401).send({ error: "User not found" });
+    }
 
-        //the student is not in the database
-        if(results[0] == null){
-            res.status(401).send({ error: "User not found" });
-        }
+    //else, the student was found so verify the rso id
+  });
 
-        //else, the student was found so verify the rso id
-    }).then(connection.query(verify_query2, [rso_id], (err, results) => {
-        if(err) throw err;
+  //verify the rso exists in the database
+  await connection.query(verify_query2, [rso_id], (err, results) => {
+    if (err) throw err;
 
-        //the rso is not in the database
-        if(results[0] == null){
-            res.status(401).send({ error: "RSO not found" });
-        }
+    //the rso is not in the database
+    if (results[0] == null) {
+      res.status(401).send({ error: "RSO not found" });
+    }
 
-        //else, the rso was found, now add row to the joins table
-    })).then(connection.query(query, [rso_id, id], (err, results) => {
-        if(err) throw err;
+    //else, the rso was found, now add row to the joins table
+  });
 
-        //successful insertion
-        res.status(200).send({message: "Successfully joined to the RSO!!"})
+  //add a row in the joins table, (student joins rso)
+  await connection.query(query, [rso_id, id], (err) => {
+    if (err) throw err;
 
-    }));
-}
+    //successful insertion
+    res.status(200).send({ message: "Successfully joined to the RSO!!" });
+  });
+};
 
-const create_rso_handler = (req, res) =>{
+const create_rso_handler = (req, res) => {
+  const { name, description, email1, email2, email3 } = req.body;
+  var id;
+  //verify the students emails are valid
+  const verify_email1 = `SELECT id FROM users WHERE email = ?`;
+  const verify_email2 = `SELECT id FROM users WHERE email = ?`;
+  const verify_email3 = `SELECT id FROM users WHERE email = ?`;
+  const query = `INSERT INTO rso (name, description, id) VALUES (?, ?, ?)`;
 
-    const {name, description, email1, email2, email3} = req.body;
-    var id;
-    //verify the students emails are valid
-    const verify_email1= `SELECT id FROM users WHERE email = ?`;
-    const verify_email2= `SELECT id FROM users WHERE email = ?`;
-    const verify_email3= `SELECT id FROM users WHERE email = ?`;
-    const query = `INSERT INTO rso (name, description, id) VALUES (?, ?, ?)`;
+  connection.query(verify_email1, [email1], (err, results) => {
+    if (err) throw err;
 
-    connection.query(verify_email1, [email1], (err, results) => {
-        if(err) throw err;
+    //the user is not in the database
+    if (results[0] == null) {
+      res.status(401).send({ error: "User not found" });
+    }
+    id = results[0];
+  });
+  connection.query(verify_email2, [email2], (err, results) => {
+    if (err) throw err;
 
-        //the user is not in the database
-        if(results[0] == null){
-            res.status(401).send({ error: "User not found" });
-        }
-        id = results[0];
+    //the user is not in the database
+    if (results[0] == null) {
+      res.status(401).send({ error: "User not found" });
+    }
+  });
+  connection.query(verify_email3, [email3], (err, results) => {
+    if (err) throw err;
 
-    })
-    connection.query(verify_email2, [email2], (err, results) => {
-        if(err) throw err;
+    //the user is not in the database
+    if (results[0] == null) {
+      res.status(401).send({ error: "User not found" });
+    }
+  });
+  connection.query(query, [name, description, id], (err, results) => {
+    if (err) throw err;
+    //successful insertion
+    res.status(200).send({ message: "Successfully created an RSO!!" });
+  });
+};
 
-        //the user is not in the database
-        if(results[0] == null){
-            res.status(401).send({ error: "User not found" });
-        }
-    })
-    connection.query(verify_email3, [email3], (err, results) => {
-        if (err) throw err;
-
-        //the user is not in the database
-        if(results[0] == null){
-            res.status(401).send({error: "User not found"});
-        }
-    })
-    connection.query(query, [name, description, id], (err, results)=>{
-        if (err) throw err;
-        //successful insertion
-        res.status(200).send({message: "Successfully created an RSO!!"})
-    });
-
-
-}
-
-module.exports = {join_rso_handler, create_rso_handler};
+module.exports = { join_rso_handler, create_rso_handler };
