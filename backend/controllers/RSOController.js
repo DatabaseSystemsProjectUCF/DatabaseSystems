da53f2b7368136f1d67040366c2969409935d619
Juan Guarnizo <88952365+Ozymandias0912@users.noreply.github.com>
2023-03-21 22:11:23 -0400

    Deleted unnecessary lines

--- backend/controllers/RSOController.js
+++ backend/controllers/RSOController.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-<<<<<<< Updated upstream
-const connection = require("./../Database");
-//const connection = require("./../DatabaseJuan");
-=======
 // const connection = require("./../Database");
 const connection = require("./../DatabaseJuan");
-const { query } = require("./../DatabaseJuan");
->>>>>>> Stashed changes
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
